5622830e6f708518e80554a4f2dba0ff495f917a
DevashishRoyal <devashish.royalswebtech@gmail.com>
2025-12-02 22:59:20 +0530

    scrollOn TOp

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-white">
+      <ScrollToTop />
       <Header />
       {children}
       <Footer />
